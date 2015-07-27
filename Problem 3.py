3410f07b6f900052bb76663d50e84a9d13ba8fc6
Jin <janus@gwmail.gwu.edu>
2015-07-27 02:51:50 -0400

    Update for Problem 3
    Largest Prime Factor

---
+++ Problem 3.py
@@ -0,0 +1,26 @@
+__author__ = 'januschou'
+
+import math
+
+# Determine whether a integer is prime number
+def IsPrime(n):
+    #starting from 3 and only considering odd numbers,
+    for i in xrange(3,int(math.floor(math.sqrt(n))),2):
+        if n % i == 0:
+            return False
+    return True
+
+
+def LargestPrimeFactor(n):
+    LargestFactor = 0
+    for i in xrange(3,int(math.floor(math.sqrt(n))),2):
+        if n % i == 0 and IsPrime(i):
+            LargestFactor = i
+
+    if LargestFactor == 0:
+        return n
+    else:
+        return  LargestFactor
+
+
+LargestPrimeFactor(600851475143)
